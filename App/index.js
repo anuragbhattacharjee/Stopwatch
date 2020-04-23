c44eb34ef7dbaeee7497a4f97807cb415cbb2185
Anurag B <anurag@chatleads.io>
2020-04-23 17:37:52 +0600

    First commit; a working stopwatch;

---
+++ App/index.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+
+const screen = Dimensions.get("window");
+const formatNumber = (number) => `0${number}`.slice(-2);
+const getRemaining = (time) => {
+  const mins = Math.floor(time / 60);
+  const secs = time - mins * 60;
+  return { mins: formatNumber(mins), secs: formatNumber(secs) };
+};
+
+export default function App() {
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
+
+  toggle = () => {
+    setIsActive(!isActive);
+  };
+
+  reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setRemainingSecs((remainingSecs) => remainingSecs + 1);
+      }, 1000);
+    } else if (!isActive && remainingSecs !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, remainingSecs]);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content"></StatusBar>
+      <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+      <TouchableOpacity onPress={toggle} style={styles.button}>
+        <Text style={styles.buttonText}>{isActive ? "Pause" : "Start"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={reset}
+        style={[styles.button, styles.buttonReset]}
+      >
+        <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#07121B",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    borderWidth: 10,
+    borderColor: "#B9AAFF",
+    width: screen.width / 2,
+    height: screen.width / 2,
+    borderRadius: screen.width / 2,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonReset: {
+    marginTop: 20,
+    borderColor: "#FF851B",
+  },
+  buttonText: {
+    fontSize: 45,
+    color: "#89AAFF",
+  },
+  buttonTextReset: {
+    color: "#FF851B",
+  },
+  timerText: {
+    color: "#fff",
+    fontSize: 90,
+    marginBottom: 20,
+  },
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# Stopwatch
+My first react native app
